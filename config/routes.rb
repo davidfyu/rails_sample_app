4007f343a44232f6d431a565be0053a62b14812f
david yu <davidfyu@umich.edu>
2020-02-27 13:04:49 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/signup' => 'users#new'
   get '/help' => 'static_pages#help'
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
+  get '/signup' => 'users#new'
+	post '/signup' => 'users#create'
+	resources :users
 end
